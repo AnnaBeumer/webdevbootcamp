00d573ebf9f3821b0c431898c7f255f1f1083be1
Anna Beumer <annabeumer@MacBook-Air-van-Anna.local>
2020-02-05 23:17:02 +0100

    updated yelpcamp

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -2,6 +2,38 @@ require("dotenv").config({ path: __dirname + "/.env" });
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp_camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Granite Hill",
+//     image:
+//       "https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_960_720.jpg",
+//     description:
+//       "This is a huge granite hill, no bathrooms. No water. Beautiful granite!"
+//   },
+//   function(err, campground) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Newly created campground");
+//       console.log(campground);
+//     }
+//   }
+// );
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -10,38 +42,61 @@ app.get("/", function(req, res) {
   res.render("landing");
 });
 
-var campgrounds = [
-  {
-    name: "Salmon Creek",
-    image:
-      "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022_960_720.jpg"
-  },
-  {
-    name: "Granite Hill",
-    image:
-      "https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_960_720.jpg"
-  },
-  {
-    name: "Mountain Goat's Rest",
-    image:
-      "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_960_720.jpg"
-  }
-];
+// var campgrounds = [
+//   {
+//     name: "Salmon Creek",
+//     image:
+//       "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022_960_720.jpg"
+//   },
+//   {
+//     name: "Granite Hill",
+//     image:
+//       "https://cdn.pixabay.com/photo/2016/11/29/04/17/bonfire-1867275_960_720.jpg"
+//   },
+//   {
+//     name: "Mountain Goat's Rest",
+//     image:
+//       "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_960_720.jpg"
+//   }
+// ];
 
 app.get("/campgrounds", function(req, res) {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  Campground.find({}, function(err, allCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { campgrounds: allCampgrounds });
+    }
+  });
 });
 
 app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = { name: name, image: image };
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+  var desc = req.body.description;
+  var newCampground = { name: name, image: image, description: desc };
+  Campground.create(newCampground, function(err, newlyCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+  // campgrounds.push(newCampground);
 });
 
 app.get("/campgrounds/new", function(req, res) {
   res.render("new.ejs");
+});
+
+app.get("/campgrounds/:id", function(req, res) {
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("show", { campground: foundCampground });
+    }
+  });
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
--- YelpCamp/cats.js
+++ YelpCamp/cats.js
@@ -11,3 +11,43 @@ var catSchema = new mongoose.Schema({
 });
 
 var Cat = mongoose.model("Cat", catSchema);
+
+// var george = new Cat({
+//   name: "Mrs. Norris",
+//   age: 7,
+//   temperament: "Evil"
+// });
+
+// george.save(function(err, cat) {
+//   if (err) {
+//     console.log("Something went wrong");
+//   } else {
+//     console.log("We just saved a cat to the DB");
+//     console.log(cat);
+//   }
+// });
+
+Cat.create(
+  {
+    name: "Snow White",
+    age: 15,
+    temperament: "Bland"
+  },
+  function(err, cat) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(cat);
+    }
+  }
+);
+
+Cat.find({}, function(err, cats) {
+  if (err) {
+    console.log("Oh no, error!");
+    console.log(err);
+  } else {
+    console.log("All the cats");
+    console.log(cats);
+  }
+});
